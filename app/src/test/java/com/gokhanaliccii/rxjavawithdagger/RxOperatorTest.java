44a84079f9f23293c87c379f5a41aff2c64d5f39
Gokhan Alıcı <gokhanaliccii@gmail.com>
2018-05-13 19:35:31 +0300

    Rx subject types tested

--- app/src/test/java/com/gokhanaliccii/rxjavawithdagger/RxOperatorTest.java
+++ app/src/test/java/com/gokhanaliccii/rxjavawithdagger/RxOperatorTest.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -107,17 +109,26 @@ public class RxOperatorTest {
     public void should_ZipTwoObservablesCorrectly() throws Exception {
         TestObserver<String> stringTestSubscriber = new TestObserver<>();
 
+        List<Observable<Integer>> obs = new LinkedList<>();
+
         Observable<Integer> oddNumberObservable =
-                Observable.range(1, 3).filter(new Predicate<Integer>() {
+                Observable.range(1, 30).subscribeOn(Schedulers.io()).filter(new Predicate<Integer>() {
                     @Override
                     public boolean test(Integer integer) throws Exception {
 
                         String threadName = Thread.currentThread().getName();
+                        if (integer == 4)
+                            throw new NullPointerException();
 
-                        System.out.println("called(" + threadName + ") 1 *" + integer);
+                        System.out.println("called1(" + threadName + ") 1 *" + integer);
                         return integer % 2 != 0;
                     }
-                });
+                }).doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+
+                    }
+                }).subscribeOn(Schedulers.io());
 
         Observable<Integer> evenNumberObservable = Observable.range(1, 10)
                 //.delay(2, TimeUnit.SECONDS)
@@ -125,9 +136,19 @@ public class RxOperatorTest {
                     @Override
                     public boolean test(Integer integer) throws Exception {
                         String threadName = Thread.currentThread().getName();
+                        System.out.println("called2(" + threadName + ") 2 *" + integer);
 
-                        System.out.println("called(" + threadName + ") 2 *" + integer);
+                        if (integer == 5)
+                            throw new NullPointerException();
+
                         return integer % 2 == 0;
+                    }
+                }).onErrorReturn(new Function<Throwable, Integer>() {
+                    @Override
+                    public Integer apply(Throwable throwable) throws Exception {
+                        System.out.println("hata 2");
+
+                        return -1;
                     }
                 }).observeOn(Schedulers.computation()).map(new Function<Integer, Integer>() {
                     @Override
@@ -135,29 +156,85 @@ public class RxOperatorTest {
 
                         String threadName = Thread.currentThread().getName();
 
-                        System.out.println("called(" + threadName + ") 2 map *" + integer);
+
+                        System.out.println("called2(" + threadName + ") 2 map *" + integer);
                         return integer;
                     }
-                }).observeOn(Schedulers.newThread());
+                });
+
+        Observable.mergeDelayError(oddNumberObservable,evenNumberObservable)
+                .doOnError(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                System.out.println("fddfdf");
+            }
+        }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+
+                System.out.println("\t r:" + integer);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                System.out.println("ugurcan cıkolatasız olmaz ");
+            }
+        });
 
 
+     /*   oddNumberObservable.zipWith(evenNumberObservable,new BiFunction<Integer, Integer, String>() {
+            @Override
+            public String apply(Integer integer, Integer integer2) throws Exception {
+
+                String s = "?    l" + integer + "   r:" + integer2;
+                System.out.println(s);
+                return s;
+            }
+        }).subscribeOn(Schedulers.io()).doOnNext(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println("birleşmemişs:"+s);
+            }
+        }).doOnError(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+
+            }
+        }).subscribe();*/
+/*
         Observable.zip(oddNumberObservable, evenNumberObservable, new BiFunction<Integer, Integer, String>() {
             @Override
             public String apply(Integer integer, Integer integer2) throws Exception {
 
-                String s = "    l" + integer + "   r:" + integer2;
+                String s = "?    l" + integer + "   r:" + integer2;
                 System.out.println(s);
                 return s;
             }
-        }).subscribe(stringTestSubscriber);
+        }).subscribeOn(Schedulers.io()).subscribe(stringTestSubscriber);*/
 
-        Thread.sleep(3000);
+        Thread.sleep(300000);
     }
 
     @Test
     public void should_PrintDifferentTimeStampEveryTime() throws Exception {
 
-        Observable<Long> just = Observable.just(-2l);
+        Observable<Long> just = Observable.rangeLong(1, 20).map(new Function<Long, Long>() {
+            @Override
+            public Long apply(Long aLong) throws Exception {
+                Thread.sleep(300);
+                return aLong;
+            }
+        }).subscribeOn(Schedulers.computation());
+
+        Observable<String> just2 =
+                Observable.rangeLong(1, 20).map(new Function<Long, String>() {
+                    @Override
+                    public String apply(Long aLong) throws Exception {
+                        Thread.sleep(300);
+
+                        return "hey*" + aLong;
+                    }
+                }).subscribeOn(Schedulers.computation());
 
         Observable<Long> longSecond = Observable.interval(2, TimeUnit.SECONDS).doOnNext(new Consumer<Long>() {
             @Override
@@ -167,14 +244,6 @@ public class RxOperatorTest {
             }
         });
 
-
-        Observable.concat(just, longSecond).doOnNext(new Consumer<Long>() {
-            @Override
-            public void accept(Long aLong) throws Exception {
-
-                System.out.println("qq" + aLong);
-            }
-        }).subscribe();
 
         Thread.sleep(4000);
     }
